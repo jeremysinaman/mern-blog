5fef83cf0b6e28e2675b602cfb7e54e1bbff3767
Jeiy <jeremy.sinaman@gmail.com>
2021-10-28 23:32:18 +0200

    node , mongo , Login and register routes

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
-
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGO_URL, {
     
 }).then(console.log("Connected to MongoDB"))
 .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
     console.log("Backend is running");
