8bce5a78244cec75dfdb95133d06e0f1249c9590
Jeiy <jeremy.sinaman@gmail.com>
2021-10-28 16:18:22 +0200

    login and register page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import TopBar from "./components/topbar/TopBar";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 
 function App() {
   return (
     <>
     <TopBar/>
-    <Settings/>
+    <Register/>
     </>
   );
 }
