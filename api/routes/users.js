2fe47434089394d50cec7b2faa60659f67c79373
Jeiy <jeremy.sinaman@gmail.com>
2021-10-30 18:54:12 +0200

    posts, request query

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const Post = require("../models/User");
 const bcrypt = require("bcrypt");
-const { deleteModel } = require("mongoose");
+
 
 // UPDATE
 router.put("/:id", async (req,res) => {
