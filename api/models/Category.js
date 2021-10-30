bc26e6b117e38d8e5c94e6e2adf21287db46c71f
Jeiy <jeremy.sinaman@gmail.com>
2021-10-30 19:29:27 +0200

    api done , GET and POST categories, upload file REST API

--- api/models/Category.js
+++ api/models/Category.js
@@ -10,4 +10,4 @@ const CategorySchema = new mongoose.Schema(
 },{ timestamps:true }
 );
 
-module.exports = mongoose.model("Post", CategorySchema);
+module.exports = mongoose.model("Category", CategorySchema);
